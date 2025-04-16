a276cbb50c7d1b973168f46bfabac2d1f1fcc49d
Mirabbos-08 <sobitjonovmirabbos1@gmail.com>
2025-04-16 19:30:22 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+   <div className='container'>
+
+   <h1>Hello world</h1>
+   <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus labore nam necessitatibus blanditiis hic, deserunt quibusdam iure, adipisci laudantium, totam eum voluptatum natus excepturi quam sed eius voluptatem fuga impedit. Similique dolore sapiente at et cum, atque neque consectetur est quasi facere! Error nesciunt unde nostrum repellat eveniet officiis dolor!</p>
+   <ul>
+    <li> Html </li>
+    <li> Css </li>
+    <li> React </li>
+    <li> JavaScript </li>
+   </ul>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur numquam aliquam, ad, aperiam eaque id quae suscipit eos magnam laboriosam rerum veritatis. Quos est odit iusto modi quisquam. Commodi maxime sapiente illo quasi? Id, obcaecati est odit accusamus excepturi recusandae. Itaque quia quos dolorum, et eius doloribus magnam expedita iusto.</p>
+   <ul>
+    <li> Html </li>
+    <li> Css </li>
+    <li> React </li>
+    <li> JavaScript </li>
+   </ul>
+   </div>
+     
+    
+    
+    
+    </>
+  )
+}
+
+export default App
